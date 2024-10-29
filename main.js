a269b94b69e7ad2bcbdeb461ade69d297cbcec4a
Yerayzl1 <146326631+Yerayzl1@users.noreply.github.com>
2024-10-29 20:13:40 +0100

    Main Page responsive

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import './style.css'
+// Mobile Menu Toggle Script
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      mainColor: '#FF0000',
+      secondaryColor: '#FF7700',
+      terciaryColor: '#8CFF00',
+      linkColor: '#00B7FF',
+      black: '#000000',
+      white: '#FFFFFF',
+    },
+    extend: {
+      backgroundImage: {
+        'background': 'linear-gradient(0deg, #990000 0%, #1F1812 29%, #994C08 69%, #FF7700 100%)',
+        'plus': 'linear-gradient(0deg, #FF7700 0%, #FF3636 50%, #FF6E00 100%)',
+      },
+    },
   },
   plugins: [],
 };
