b82e060af4ac99d7ff880494e8006f9a43dbd055
Sergi Aylagas <sergi.aylagas@cirvianum.cat>
2024-11-05 20:19:49 +0100

    vercel vonfig v.3

--- vite.config.js
+++ vite.config.js
@@ -2,41 +2,31 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  root: "./", // Directori root de Vite
-  base: "./", // Rutes relatives per als assets
+  root: "./",
+  base: "./",
   build: {
-    outDir: "dist", // Directori de sortida per al build
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "index.html"), // Pàgina principal
+        main: path.resolve(__dirname, "index.html"),
         aboutUs: path.resolve(__dirname, "src/about/about-us.html"),
         help: path.resolve(__dirname, "src/about/help.html"),
         rentmotionPlus: path.resolve(__dirname, "src/about/rentmotion-plus.html"),
-        contact: path.resolve(
-          __dirname,
-          "src/contact.html"
-        ),
-        download: path.resolve(
-          __dirname,
-          "src/downloadPage.html"
-        ),
-        login: path.resolve(
-          __dirname,
-          "src/login.html"
-        ),
-        registers: path.resolve(
-          __dirname,
-          "src/register.html"
-        ),
+        contact: path.resolve(__dirname, "src/contact.html"),
+        download: path.resolve(__dirname, "src/downloadPage.html"),
+        login: path.resolve(__dirname, "src/login.html"),
+        registers: path.resolve(__dirname, "src/register.html"),
         equipment: path.resolve(__dirname, "src/services/equipment.html"),
         events: path.resolve(__dirname, "src/services/events.html"),
         ubications: path.resolve(__dirname, "src/services/ubications.html"),
+        mainJS: path.resolve(__dirname, "src/main.js"),
+        mapJs: path.resolve(__dirname, "assets/js/map.js"),
       },
     },
   },
   server: {
     watch: {
-      usePolling: true, // Per assegurar que els canvis es detecten en alguns entorns
+      usePolling: true,
     },
   },
 });
