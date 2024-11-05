53a493e503a61c749aee762891129b1922d91357
Sergi Aylagas <sergi.aylagas@cirvianum.cat>
2024-11-05 20:09:29 +0100

    vercel config

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  root: "./", // Directori root de Vite
+  base: "./", // Rutes relatives per als assets
+  build: {
+    outDir: "dist", // Directori de sortida per al build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"), // Pàgina principal
+        aboutUs: path.resolve(__dirname, "src/about/about-us.html"),
+        help: path.resolve(__dirname, "src/about/help.html"),
+        rentmotionPlus: path.resolve(__dirname, "src/about/rentmotion-plus.html"),
+        contact: path.resolve(
+          __dirname,
+          "src/contact.html"
+        ),
+        download: path.resolve(
+          __dirname,
+          "src/download.html"
+        ),
+        login: path.resolve(
+          __dirname,
+          "src/login.html"
+        ),
+        registers: path.resolve(
+          __dirname,
+          "src/register.html"
+        ),
+        equipment: path.resolve(__dirname, "src/services/equipment.html"),
+        events: path.resolve(__dirname, "src/services/events.html"),
+        ubications: path.resolve(__dirname, "src/services/ubications.html"),
+      },
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true, // Per assegurar que els canvis es detecten en alguns entorns
+    },
+  },
+});
